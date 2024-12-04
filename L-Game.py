4ba348812f5d0b4662761605fd2a6d57508e6df8
JDyer4 <james.dyer.dyer@gmail.com>
2024-12-03 23:22:39 -0800

    bruh idk anymore

--- L-Game.py
+++ L-Game.py
@@ -2,6 +2,8 @@ import copy
 import sys
 import random
 import time
+import heapq
+import traceback
 
 initial_state = {
     'player1': {'position': (2, 0), 'orientation': 'W'},
@@ -9,15 +11,23 @@ initial_state = {
     'neutral': [(0, 0), (3, 3)],
     'turn': 1,  # 1 for Player 1's turn, 2 for Player 2's turn
     'bypass_player': None,
-    'game_mode': 'PvP',
-    'depth': 2,
 }
 
 global_vars = {
     'moves': 0,
     'cmd_args': None,
-    'random_cpu': False,
-    'execution_times': []
+    'random_cpu': False, # If player2 is a CPU, then it will play moves at random
+    'execution_times': [],
+    'game_mode': 'PvP',
+    'depth': 2, # The max depth for the minimax algorithm
+    
+    'nodes_evaluated': 0,
+
+    'debug': False, # If true, then allows all debug statements to be printed.
+
+    'priorityq': False,
+
+    'stepbystep': False
 }
 
 orientations = {
@@ -246,9 +256,9 @@ def playerTurn(state):
             continue
 
      # Proceed to next turn
-    if state['game_mode'] == 'PvP':
+    if global_vars['game_mode'] == 'PvP':
         playerTurn(state)
-    elif state['game_mode'] == 'PvC':
+    elif global_vars['game_mode'] == 'PvC':
         computerTurn(state)
     else:
         raise Exception("Something went wrong with the gamemode selection")
@@ -276,7 +286,13 @@ def computerTurn(state):
 
     board = buildBoard(state)
 
-    if initial_state['game_mode'] == 'PvC':
+        
+    if (player == 1):
+        global_vars['moves'] += 1
+        print("\n--------------------")
+        print("Turn", global_vars['moves'])
+
+    if global_vars['game_mode'] == 'PvC':
         print("Computer's turn...")
     else:
         print(f"\nComputer {player}'s turn...")
@@ -287,48 +303,76 @@ def computerTurn(state):
         terminalState(player)
         return
 
-    _, best_action = minimax(state, state['depth'], float('-inf'), float('inf'), True)
+    _, best_action = minimax(state, global_vars['depth'], float('-inf'), float('inf'), True)
+    
 
-    if best_action is None:
-        terminalState(player)
-        return
 
     # Player 2 is random
     if player == 2 and global_vars['random_cpu'] == True:
-        list_legal_actions = list(legal_actions)
-        best_action = list_legal_actions[random.randint(0, len(list_legal_actions) - 1)]
+        best_action = random.choice(list(legal_actions))
+    else:
+        if best_action is None and legal_actions: # When minimax fails to find an action, choose the best action based of evaluate_state()
+            action_priority_queue = []
+            for action in legal_actions:
+                new_state = apply_action(state, action)
+                heuristic_score = evaluate_state(new_state)
+                priority = -heuristic_score
+                heapq.heappush(action_priority_queue, (priority, len(action_priority_queue), action))  
+            best_action = heapq.heappop(action_priority_queue)[2]
+            if (global_vars['debug']): 
+                print('CRISES AVERTED')
+                input()
+        
+        elif best_action is None:
+            terminalState(player)
+            return
     
     # Update state with the best action
     state = apply_action(state, best_action)
 
     # Print the computer's move in the required format
-    if initial_state['game_mode'] == 'PvC':
+    
+
+    
+    if global_vars['game_mode'] == 'PvC':
         print("Computer's move:")
     else:
         print(f"Computer {player}'s move:")
     print(format_move(best_action))
 
-    global_vars['moves'] += 1
-    if (player == 1):
-        print("\n--------------------")
-        print("Turn", global_vars['moves'])
 
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print(f"Execution Time: {round(execution_time, 2)} seconds")
-    if global_vars['random_cpu'] == True and player == 1:
-        global_vars['execution_times'].append(execution_time)
-    elif global_vars['random_cpu'] == False:
-        global_vars['execution_times'].append(execution_time)
+    if (global_vars['debug']):
+        print("Nodes evalutated:", global_vars['nodes_evaluated'])
+        global_vars['nodes_evaluated'] = 0
 
-    numbers = global_vars['execution_times']
-    avg = sum(numbers) / len(numbers)
-    print(f"Avg excecution time: {round(avg, 2)} seconds")
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Execution Time: {round(execution_time, 2)} seconds")
+        if global_vars['random_cpu'] == True and player == 1 and global_vars['game_mode'] == 'CvC':
+            global_vars['execution_times'].append(execution_time)
+        elif global_vars['random_cpu'] == False or global_vars['game_mode'] == 'PvC':
+            global_vars['execution_times'].append(execution_time)
+
+        numbers = global_vars.get('execution_times', [])
+
+        if numbers:  # Check if the list is not empty
+            avg = sum(numbers) / len(numbers)
+            if global_vars['game_mode'] == 'PvC':
+                print(f"Avg execution time (computer): {round(avg, 2)} seconds")
+            else:
+                print(f"Avg execution time (per player): {round(avg, 2)} seconds")
+            print(f"Total runtime: {round(sum(numbers), 2)} seconds")
+        else:
+            print("No execution times recorded. Unable to compute averages or total runtime.")
+
     
     # Proceed to next turn
-    if state['game_mode'] == 'PvC':
+    if global_vars['stepbystep'] and global_vars['game_mode'] == 'CvC':
+        input()
+
+    if global_vars['game_mode'] == 'PvC':
         playerTurn(state)
-    elif state['game_mode'] == 'CvC':
+    elif global_vars['game_mode'] == 'CvC':
         computerTurn(state)
     else:
         raise Exception("Something went wrong with the gamemode selection")
@@ -340,11 +384,31 @@ def evaluate_state(state):
     Positive values favor the current player, while negative values favor the opponent.
     """
 
-    standardized_loss_states = set(())
-
     player = state['turn']
     opponent = 1 if player == 2 else 2
 
+    # Any of these 14 predetermined states is a garenteed loss
+    # against a optimal opponent and therefor avoided
+    """standardized_loss_states = {"...N..212221.N11",
+                                "..112221..21.NN.",
+                                "...2.222N111.N.1",
+                                "..2N..2.N221.111",
+                                "..112221N.21..N.",
+                                ".....222N21N111.",
+                                "...N..212221N.11",
+                                "...NN.212221..11",
+                                ".....222N2.1N111",
+                                "..112221..21.N.N",
+                                "..112221..21..NN",
+                                "..112221N.21.N..",
+                                "...1N2N1.211.22.",
+                                "...N222.21N..111",}
+
+    # Check for loss states for player
+    normal_board = normalize_board(buildBoard(state))
+    if normal_board in standardized_loss_states:
+        return float('-inf')"""
+    
     # Get legal moves for both players
     state['turn'] = player
     player_moves = len(getLegalActions(state))
@@ -429,11 +493,29 @@ def build_symmetries(board):
     symmetries.append(reflected_d2)
 
     # Print all boards for verification
-    for idx, sym_board in enumerate(symmetries):
+    """for idx, sym_board in enumerate(symmetries):
         print(f"Symmetry {idx + 1}:")
-        printBoard(sym_board)
+        printBoard(sym_board)"""
 
-    return symmetries
+    lexicographical_strings = [board_to_lex_string(symmetry) for symmetry in symmetries]
+
+    return lexicographical_strings
+
+def normalize_board(board):
+    """
+    Generates all symmetries of a 4x4 board, sorts them lexicographically, 
+    and returns the lexicographically smallest representation of the board.
+    """
+    strings = build_symmetries(board)
+    strings.sort()
+    return strings[0]
+
+def board_to_lex_string(board):
+    """
+    Convert a 4x4 board into a lexicographical string.
+    Helper for build_symmetries()
+    """
+    return ''.join(''.join(row) for row in board)
 
 def minimax(state, depth, alpha, beta, maximizing_player):
     """
@@ -444,7 +526,7 @@ def minimax(state, depth, alpha, beta, maximizing_player):
     if depth == 0 or is_terminal(state):
         return evaluate_state(state), None
 
-    legal_actions = getLegalActions(state)
+    legal_actions = getLegalActions(state) # Redundant line of code?
     if not legal_actions:
         return evaluate_state(state), None
 
@@ -452,7 +534,9 @@ def minimax(state, depth, alpha, beta, maximizing_player):
 
     if maximizing_player:
         max_eval = float('-inf')
+        if (global_vars['priorityq']): legal_actions = prioritize_actions(state, legal_actions, True) # alpha-beta huerestic
         for action in legal_actions:
+            global_vars['nodes_evaluated'] += 1
             new_state = apply_action(state, action)
             eval, _ = minimax(new_state, depth - 1, alpha, beta, False)
             if eval > max_eval:
@@ -464,7 +548,9 @@ def minimax(state, depth, alpha, beta, maximizing_player):
         return max_eval, best_action
     else:
         min_eval = float('inf')
+        if (global_vars['priorityq']): legal_actions = prioritize_actions(state, legal_actions, False) # alpha-beta huerestic
         for action in legal_actions:
+            global_vars['nodes_evaluated'] += 1
             new_state = apply_action(state, action)
             eval, _ = minimax(new_state, depth - 1, alpha, beta, True)
             if eval < min_eval:
@@ -474,6 +560,20 @@ def minimax(state, depth, alpha, beta, maximizing_player):
             if beta <= alpha:
                 break  # Alpha cut-off
         return min_eval, best_action
+
+def prioritize_actions(state, legal_actions, maximizing_player):
+    """
+    Assign priorities to legal actions based on a heuristic evaluation of the resulting state.
+    """
+    action_priority_queue = []
+    for action in legal_actions:
+
+        new_state = apply_action(state, action)
+        heuristic_score = evaluate_state(new_state)
+        # Use negative scores for maximizing player (to simulate max-heap behavior with min-heap)
+        priority = -heuristic_score if maximizing_player else heuristic_score
+        heapq.heappush(action_priority_queue, (priority, len(action_priority_queue), action))  
+    return [heapq.heappop(action_priority_queue)[2] for _ in range(len(action_priority_queue))]
 
 def apply_action(state, action):
     """
@@ -535,12 +635,36 @@ def getPlayerInput(playerID, state, board):
     while True:
         try:
             user_input = '0'
-            while (user_input == '0'):
+            while (user_input == '0' or user_input == 'help()' or user_input == 'save()'):
                 user_input = input("Player " + str(playerID) + " to move...\n").strip() 
 
                 # Debug command
                 if (user_input == '0'):
                     printLegalActions(state, board)
+
+                if (user_input == 'help()'):
+                    legal_actions = getLegalActions(state)
+                    random_move = random.choice(list(legal_actions))
+                    (x, y), orient = random_move[0], random_move[1]
+                    if (random_move[2]):
+                        (a, b), (c, d) = random_move[2], random_move[3]
+                        print(f"\nTry the move: {x + 1} {y + 1} {orient} {a + 1} {b + 1} {c + 1} {d + 1}\n")
+                    else:
+                        print(f"\nTry the move: {x + 1} {y + 1} {orient}\n")
+
+                if (user_input == 'save()'):
+                    (x1, y1) = state['player1']['position']
+                    x1, y1 = x1 + 1, y1 + 1
+                    (orient1) = state['player1']['orientation']
+                    (x2, y2) = state['player2']['position']
+                    x2, y2 = x2 + 1, y2 + 1
+                    (orient2) = state['player2']['orientation']
+                    [(a, b), (c, d)] = state['neutral']
+                    a, b, c, d = a + 1, b + 1, c + 1, d + 1
+                    print(f"\nUse this string to start from this game state: \n{x1} {y1} {orient1} {a} {b} {c} {d} {x2} {y2} {orient2}\n")
+                
+                if (user_input == 'quit()'):
+                    sys.exit()
 
             parts = user_input.split()
             
@@ -594,6 +718,16 @@ def terminalState(losingPlayer):
     sys.exit()
 
 def mainMenu():
+
+    # Parse cmd line arguments
+    arguments = set(sys.argv)
+    if "--randomCPU" in arguments:
+        global_vars['random_cpu'] = True
+    if "--debug" in arguments:
+        global_vars['debug'] = True
+    if "--stepbystep" in arguments:
+        global_vars['stepbystep'] = True
+
     while True:
         try:
             # Display the menu and get user input
@@ -610,7 +744,7 @@ def mainMenu():
             # Validate and handle the input
             if choice == '1':
                 print("Starting Player vs Player game...")
-                initial_state['game_mode'] = 'PvP'
+                global_vars['game_mode'] = 'PvP'
                 playerTurn(initial_state)  # Start Player vs Player game
 
             elif choice == '2':
@@ -631,6 +765,9 @@ def mainMenu():
             print("\n--------------------")
             print(f"Error: {e}.\n")
 
+            if (global_vars['debug']):
+                traceback.print_exc()
+
 def start_pvc():
     while True:
         try:
@@ -644,7 +781,7 @@ def start_pvc():
             if not (starting_choice == '1' or starting_choice == '2'):
                 raise ValueError("Invalid input")
 
-            initial_state['game_mode'] = 'PvC'
+            global_vars['game_mode'] = 'PvC'
 
             if starting_choice == '1':
                 playerTurn(copy.deepcopy(initial_state))
@@ -657,7 +794,7 @@ def start_pvc():
             continue
 
 def start_cvc():
-    initial_state['game_mode'] = 'CvC'
+    global_vars['game_mode'] = 'CvC'
     computerTurn(copy.deepcopy(initial_state))
 
 def setInitialGameState():
@@ -763,23 +900,82 @@ def printLegalActions(state, board):
     for action in list_of_actions:
         if (action[3] == None):
             (x, y), orient, _, _ = action
-            print(f"{x + 1} {y + 1} {orient} {a + 1} {b + 1}")
+            print(f"{x + 1} {y + 1} {orient}")
         else:
             (x, y), orient, (a, b), (c, d) = action
             print(f"{x + 1} {y + 1} {orient} {a + 1} {b + 1} {c + 1} {d + 1}")
     printBoard(board)
 
+def parseInitialBoard():
+
+    loss_state_codes = [
+        "1 4 N 1 2 4 4 2 3 E",
+        "1 4 N 4 2 4 3 2 3 E",
+        "1 2 N 3 1 4 2 1 3 S",
+        "1 4 N 1 1 4 3 2 3 E",
+        "1 4 N 3 1 4 3 2 3 E",
+        "2 4 N 1 3 4 3 3 2 S",
+        "1 4 N 1 1 4 4 2 3 E",
+        "1 4 N 3 1 4 4 2 3 E",
+        "1 4 N 4 3 4 4 3 2 S",
+        "1 4 N 4 2 4 4 2 3 E",
+        "1 4 N 4 3 4 4 2 3 E",
+        "1 4 N 3 1 4 2 2 3 E",
+        "2 4 N 3 1 3 3 1 2 S",
+        "2 4 N 4 1 3 3 1 2 S",
+    ]
+
+    loss_states = []
+
+
+    for i, state in enumerate(loss_state_codes):
+        elements = state.split()
+
+        # Update the initial state
+        new_state = copy.deepcopy(initial_state)
+
+        x1, y1, orient1, a, b, c, d, x2, y2, orient2 = map(str, elements)
+        new_state['player1']['position'] = (int(x1) - 1, int(y1) - 1)
+        new_state['player1']['orientation'] = (orient1)
+        new_state['player2']['position'] = (int(x2) - 1, int(y2) - 1)
+        new_state['player2']['orientation'] = (orient2)
+        new_state['neutral'] = [(int(a) - 1, int(b) - 1), (int(c) - 1, int(d) -1)]
+
+        board = buildBoard(new_state)
+        print(i+1)
+        printBoard(board)
+        loss_states.append(normalize_board(board))
+        
+    return loss_states
+
+def chooseMinimaxDepth():
+    while True:
+        try:
+            # Prompt user for input and convert it to an integer
+            depth = int(input("Enter new minimax depth: "))
+            if depth <= 0:
+                raise ValueError("Depth must be an integer greater than 0.")
+            
+            # Set the global depth variable
+            global_vars['depth'] = depth
+            print(f"Minimax depth set to {depth}.")
+            break  # Exit loop once depth is successfully set
+
+        except ValueError as e:
+            print("\n--------------------")
+            print(f"Error: {e}.\n")
+            if global_vars.get('debug', False):  # Check if debug is enabled
+                import traceback
+                traceback.print_exc()
+
+
 def main():
 
-    # Parse cmd line arguments
-    arguments = set(sys.argv)
-    if "--randomCPU" in arguments:
-        global_vars['random_cpu'] = True
 
-    board = buildBoard(initial_state)
-    build_symmetries(board)
 
-    #mainMenu()
+    mainMenu()
+
+
 
 if __name__ == "__main__":
     main()
