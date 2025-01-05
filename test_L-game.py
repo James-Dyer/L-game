b1d9c8fcd99fea543f705093ed35b24686025a8c
JDyer4 <james.dyer.dyer@gmail.com>
2025-01-05 01:09:51 -0800

    finished getsecondaryorientation unit test

--- test_L-game.py
+++ test_L-game.py
@@ -4,24 +4,39 @@ from L_Game_copy import getSecondaryOrientation
 @pytest.mark.parametrize(
     "orientation, position, expected, should_succeed",
     [
-        # Test cases for orientation N
+        # Test cases for orientation N and S
         ('N', (0, 1), 'W', True),
-        ('N', (0, 2), 'W', True),
-        ('N', (0, 3), 'W', True),
         ('N', (0, 4), 'W', True),
-
-        ('N', (2, 3), 'E', True),
-        ('N', (3, 2), 'E', True),
-        ('N', (3, 1), 'E', True),
-
-        ('N', (0, 3), 'W', True),
-        ('N', (1, 3), 'W', True),
         ('N', (2, 3), 'E', True),
         ('N', (3, 3), 'E', True),
+        ('S', (0, 1), 'W', True),
+        ('S', (0, 4), 'W', True),
+        ('S', (2, 3), 'E', True),
+        ('S', (3, 3), 'E', True),
 
+        ('S', (1, 1), 'E', False),
+        ('S', (3, 0), 'W', False),
+        ('S', (2, 1), 'W', False),
         ('N', (1, 1), 'E', False),
         ('N', (3, 0), 'W', False),
         ('N', (2, 1), 'W', False),
+
+        # Test cases for orientation E and W
+        ('E', (0, 0), 'N', True),
+        ('E', (0, 1), 'N', True),
+        ('E', (2, 2), 'S', True),
+        ('E', (3, 3), 'S', True),
+        ('W', (0, 1), 'N', True),
+        ('W', (0, 4), 'S', True),
+        ('W', (2, 3), 'S', True),
+        ('W', (3, 3), 'S', True),   
+        
+        ('E', (1, 1), 'S', False),
+        ('E', (3, 0), 'S', False),
+        ('E', (2, 1), 'S', False),
+        ('W', (1, 2), 'N', False),
+        ('W', (3, 3), 'N', False),
+        ('W', (2, 3), 'N', False),
     ]
 )
 
